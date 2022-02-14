4979946839ce7d238abb0bbb88c93ebee9ea4fa1
soryu-u <collager447@gmail.com>
2022-02-14 18:02:38 +0200

    style edit

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ let todos = [
   {
     id: 1,
     taskTitle: "First task",
-    description: "just test area",
+    description: "just text description",
     done: false,
     due_date: "2022-02-15",
   },
@@ -22,16 +22,35 @@ let todos = [
   },
 ];
 
-const todoElement = document.getElementById("todo__items");
+function getCurrentTime() {
+  let now = new Date();
+  let currentTime = now.toTimeString().split(" ")[0];
+
+  return currentTime;
+}
+
+function updateClock() {
+  let clock = document.querySelector("#clock");
+  clock.innerText = getCurrentTime();
+}
+
+setInterval(updateClock, 1000);
 
 function appendTodo(todo, i) {
+  const todoElement = document.getElementById("todo__items");
   todoElement.innerHTML += `
   <li class="todo__item">
-    <div class="todo__task"> 
-        <input type="checkbox" id="task-${todo[i - 1].id}">
-        <p class="task__body">${todo[i - 1].taskTitle}</p>
-        <p class="task__description">${todo[i - 1].description}</p>
-        <p class="task__data">${todo[i - 1].due_date}</p>
+    <div class="todo__task">
+        <div class="task__main">
+            <input class="todo__checkbox" type="checkbox" id="task-${
+              todo[i - 1].id
+            }">
+            <p class="task__body">${todo[i - 1].taskTitle}</p>
+        </div>
+        <div class="task__dls">
+            <p class="task__description">${todo[i - 1].description}</p>
+            <p class="task__data">${todo[i - 1].due_date}</p>
+        </div>
     </div>
   </li>`;
 }
