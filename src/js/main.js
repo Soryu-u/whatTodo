8d745e9cb395c250dab1d979ab6b3b886a6d185e
soryu-u <collager447@gmail.com>
2022-02-15 17:58:57 +0200

    add all scripts, some styles for overdue tasks

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,21 @@ let todos = [
     title: "First task",
     description: "just text description",
     done: false,
-    due_date: "2022-02-15",
+    due_date: new Date("2022-02-15"),
   },
   {
     id: 2,
     title: "Second important task",
     description: "",
-    done: false,
-    due_date: "2022-02-15",
+    done: true,
+    due_date: new Date("2022-02-15"),
   },
   {
     id: 3,
     title: "Finish them all!",
     description: "not serious",
-    done: false,
-    due_date: "2022-02-15",
+    done: true,
+    due_date: new Date("2022-02-14"),
   },
 ];
 
@@ -56,17 +56,53 @@ function renderCalendar() {
 
 function renderTodo() {
   function appendTodo(todo, i) {
+    function isDone(i) {
+      if (todos[i].done) {
+        return "task__done";
+      } else {
+        return "";
+      }
+    }
+
+    function isOverdue(i) {
+      let currentDate = new Date().toDateString();
+      let taskDate = todos[i].due_date.toDateString();
+      if (taskDate < currentDate) {
+        return `overdue__task`;
+      } else {
+        return "";
+      }
+    }
+
+    function getDate(i) {
+      function month(i) {
+        if (todos[i].due_date.getMonth() % 0 > 0) {
+          return todos[i].due_date.getMonth() + 1;
+        } else {
+          return `0${todos[i].due_date.getMonth() + 1}`;
+        }
+      }
+
+      let date = `${todos[i].due_date.getFullYear()}-${month(i)}-${todos[
+        i
+      ].due_date.getDate()}`;
+      return date.toString();
+    }
+
     const todoElement = document.getElementById("todo__items");
+
     todoElement.innerHTML += `
-    <li class="todo__item">
+    <li class="todo__item ${isOverdue(i)}">
       <div class="todo__task">
           <div class="task__main">
-            <input class="todo__checkbox" type="checkbox" onclick="setStatus()" id="id-${todo[i].id}">
-            <p id="title-${todo[i].id}" class="task__body">${todo[i].title}</p>
+            
+            <p id="title-${todo[i].id}" class="task__body ${isDone(
+      i
+    )}" onclick="setStatus(event.target)">${todo[i].title}</p>
           </div>
-          <div class="task__dls">
+          <div class="task__dls ${isOverdue(i)}">
               <p class="task__description">${todo[i].description}</p>
-              <p class="task__data">${todo[i].due_date}</p>
+              <p class="task__data">${getDate(i)}</p>
           </div>
       </div>
     </li>`;
@@ -77,14 +113,9 @@ function renderTodo() {
   }
 }
 
-function setStatus() {
-  let checkbox = document.getElementById("todoDone");
-  let taskBody = document.getElementById("task");
-  if (checkbox.checked) {
-    taskBody.classList.toggle("task__done");
-  } else {
-    taskBody.classList.toggle("task__done");
-  }
+function setStatus(e) {
+  e.classList.toggle("task__done");
+  console.log("click", e);
 }
 
 renderTodo();
