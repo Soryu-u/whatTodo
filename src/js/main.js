0117059c2edb9607db35f34949d719b3caef73f9
soryu-u <collager447@gmail.com>
2022-02-18 16:02:49 +0200

    fix fetch err, refactor code, fix tasks render,  fix  get ListTodo, fix chandeStatus func

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const localUrl = "http://localhost:8000";
 fetch(localUrl + `/tasks`)
   .then((response) => response.json())
-  .then((tasks) => renderAllTodo(tasks))
+  .then(renderAllTodo)
   .catch(alert);
 
 function renderClock() {
@@ -44,7 +44,7 @@ function isDone(todo) {
   }
 }
 
-function isOpen() {
+function isOpenMode() {
   let openBtn = document.getElementById("open");
   if (openBtn.classList.contains("active")) {
     getOpenTodo();
@@ -68,20 +68,15 @@ function getDate(todo) {
   if (todo.due_date) {
     let taskDate = new Date(todo.due_date);
     function month() {
-      if (taskDate.getMonth() + 1 > 9) {
-        return taskDate.getMonth() + 1;
-      } else {
-        return `0${taskDate.getMonth() + 1}`;
-      }
+      return taskDate.getMonth() + 1 > 9
+        ? taskDate.getMonth() + 1
+        : `0${taskDate.getMonth() + 1}`;
     }
     function day() {
-      if (taskDate.getDate() > 9) {
-        return taskDate.getDate();
-      } else {
-        return `0${taskDate.getDate()}`;
-      }
+      return taskDate.getDate() > 9
+        ? taskDate.getDate()
+        : `0${taskDate.getDate()}`;
     }
-
     let date = `${day()}.${month()}.${taskDate.getFullYear()}`;
     return date;
   } else {
@@ -92,8 +87,10 @@ function getDate(todo) {
 function appendTodo(todo) {
   const todoElement = document.getElementById("todo__items");
 
-  todoElement.innerHTML += `
-  <li id="${todo.id}" class="todo__item">
+  todoElement.insertAdjacentHTML(
+    "beforeend",
+    `
+  <li id="${todo.id}" class="todo__item ${todo.done ? "done" : ""}">
       <div class="task__header">
         <input "
           type="checkbox"
@@ -109,43 +106,30 @@ function appendTodo(todo) {
           <p class="task__description">${todo.description}</p>
           <p class="task__date ${isOverdue(todo)}">${getDate(todo)}</p>
       </div>
-  </li>`;
+  </li>`
+  );
 }
 
+let allBtn = document.getElementById("all");
+let openBtn = document.getElementById("open");
+let ulContainer = document.querySelector("#todo__items");
+
 function renderAllTodo(todo) {
-  todo.forEach((todo) => {
-    appendTodo(todo);
-  });
+  todo.forEach(appendTodo);
 }
 
 function getAllTodo() {
-  let allBtn = document.getElementById("all");
-  let openBtn = document.getElementById("open");
-
   openBtn.classList.remove("active");
   allBtn.classList.add("active");
 
-  let tasksDone = document.querySelectorAll(".done");
-
-  tasksDone.forEach((task) => {
-    task.classList.remove("done");
-  });
+  ulContainer.classList.remove("hide-done");
 }
 
 function getOpenTodo() {
-  let allBtn = document.getElementById("all");
-  let openBtn = document.getElementById("open");
-
   openBtn.classList.add("active");
   allBtn.classList.remove("active");
 
-  let taskOpen = document.querySelectorAll(".todo__checkbox");
-
-  taskOpen.forEach((task) => {
-    if (task.checked) {
-      task.closest("li").classList.add("done");
-    }
-  });
+  ulContainer.classList.add("hide-done");
 }
 
 async function sendRequest(method, url, body) {
@@ -161,34 +145,29 @@ async function sendRequest(method, url, body) {
   return await response.json();
 }
 
-function changeStatus(e) {
-  isOpen();
-  let taskId = e.closest("li").id;
-  let checkbox = document.getElementById(taskId).querySelector("input");
-  let taskBody = document.getElementById(taskId).querySelector(".task__body");
+function changeStatus(input) {
+  let taskLi = input.closest("li");
+  let taskBody = document
+    .getElementById(taskLi.id)
+    .querySelector(".task__body");
 
-  if (!taskBody.classList.contains("checked")) {
-    sendRequest("PATCH", localUrl + `/lists/5/tasks/${taskId}`, {
-      done: true,
-    }).catch(alert);
-    taskBody.classList.toggle("checked");
-    checkbox.setAttribute("checked", true);
-  } else {
-    sendRequest("PATCH", localUrl + `/lists/5/tasks/${taskId}`, {
-      done: false,
-    }).catch(alert);
-    taskBody.classList.toggle("checked");
-    checkbox.removeAttribute("checked");
-  }
+  sendRequest("PATCH", localUrl + `/lists/5/tasks/${taskLi.id}`, {
+    done: !taskBody.classList.contains("checked") ? true : false,
+  })
+    .then((_) => taskLi.classList.toggle("done"))
+    .then((_) => taskBody.classList.toggle("checked"))
+    .catch(alert);
+
+  isOpenMode();
 }
 
-function deleteTask(e) {
-  let li = e.closest("li");
+function deleteTask(btn) {
+  let li = btn.closest("li");
   fetch(localUrl + `/lists/5/tasks/${li.id}`, {
     method: "delete",
-  });
-
-  li.remove();
+  })
+    .then((_) => li.remove())
+    .catch(alert);
 }
 
 const taskForm = document.forms["task"];
@@ -205,13 +184,15 @@ taskForm.addEventListener("submit", (event) => {
     due_date: task.due_date ? task.due_date : null,
   };
 
-  task.title
-    ? sendRequest("POST", localUrl + `/lists/5/tasks`, body)
-        .then((task) => appendTodo(task[0]))
-        .then((_) => titleForm[0].classList.remove("invalid__input"))
-        .then((_) => taskForm.reset())
-        .catch(alert)
-    : titleForm[0].classList.add("invalid__input");
+  if (task.title) {
+    sendRequest("POST", localUrl + `/lists/5/tasks`, body)
+      .then((task) => appendTodo(task[0]))
+      .then((_) => titleForm[0].classList.remove("invalid__input"))
+      .then((_) => taskForm.reset())
+      .catch(alert);
+  } else {
+    titleForm[0].classList.add("invalid__input");
+  }
 });
 
 renderClock();
